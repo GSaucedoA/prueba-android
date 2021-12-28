22cdf4f9c07754f644a0d75a45833a078a41387f
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-28 12:56:21 -0600

    Created model for popular movie

---
+++ app/src/main/java/com/example/pruebaandroid/model/PopularMovie.kt
@@ -0,0 +1,9 @@
+package com.example.pruebaandroid.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PopularMovie(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("poster_path") val posterPatch: String
+)
