506ca21ceb6b46795daec090df979c6557f61ec1
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-28 15:39:35 -0600

    Typo

--- app/src/main/java/com/example/pruebaandroid/model/PopularMovie.kt
+++ app/src/main/java/com/example/pruebaandroid/model/PopularMovie.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class PopularMovie(
     @SerializedName("id") val id: Int,
     @SerializedName("title") val title: String,
-    @SerializedName("poster_path") val posterPatch: String
+    @SerializedName("poster_path") val posterPath: String
 )
