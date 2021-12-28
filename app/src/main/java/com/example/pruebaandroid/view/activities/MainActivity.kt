38488b656c5ef8baad262964242ab3c3fb6756c7
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-28 15:14:19 -0600

    Calculated span count dynamically

--- app/src/main/java/com/example/pruebaandroid/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/pruebaandroid/view/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.pruebaandroid.view.activities
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.window.layout.WindowMetricsCalculator
 import com.example.pruebaandroid.R
 import com.example.pruebaandroid.databinding.ActivityMainBinding
 import com.example.pruebaandroid.model.PopularMovie
@@ -11,6 +15,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        updateSpanCount()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +37,36 @@ class MainActivity : AppCompatActivity() {
                     PopularMovie(0, "1", ""),
                     PopularMovie(0, "2", ""),
                     PopularMovie(0, "3", ""),
+                    PopularMovie(0, "4", ""),
+                    PopularMovie(0, "1", ""),
+                    PopularMovie(0, "2", ""),
+                    PopularMovie(0, "3", ""),
+                    PopularMovie(0, "4", ""),
+                    PopularMovie(0, "1", ""),
+                    PopularMovie(0, "2", ""),
+                    PopularMovie(0, "3", ""),
                     PopularMovie(0, "4", "")
                 )
             )
         }
+        updateSpanCount()
+    }
+
+    private fun updateSpanCount() {
+        (binding.recyclerView.layoutManager as GridLayoutManager).spanCount = getDynamicSpanCount()
+    }
+
+    private fun getDynamicSpanCount(): Int {
+        val widthInDP: Float
+        val density = resources.displayMetrics.density
+        val windowMetrics = WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(this)
+        val currentBounds = windowMetrics.bounds
+        val widthInPX = currentBounds.width()
+
+        Toast.makeText(this, widthInPX.toString(), Toast.LENGTH_SHORT).show()
+
+        widthInDP = widthInPX / density
+
+        return (widthInDP / 189).toInt()
     }
 }
