9d234ee6726c22212541fdcb6afc6f57f638669a
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-28 15:37:24 -0600

    Typo

--- app/src/main/java/com/example/pruebaandroid/businesslogic/respositories/PopularMovieApiRepoImpl.kt
+++ app/src/main/java/com/example/pruebaandroid/businesslogic/respositories/PopularMovieApiRepoImpl.kt
@@ -3,14 +3,14 @@ package com.example.pruebaandroid.businesslogic.respositories
 
 import com.example.pruebaandroid.BuildConfig
 import com.example.pruebaandroid.businesslogic.api.MovieDataBaseApiService
-import com.example.pruebaandroid.model.PoPularMovieResponse
+import com.example.pruebaandroid.model.PopularMovieResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class PopularMovieApiRepoImpl @Inject constructor(private val apiService: MovieDataBaseApiService) :
     PopularMovieApiRepo {
-    override suspend fun getPopularMovieList(): PoPularMovieResponse =
+    override suspend fun getPopularMovieList(): PopularMovieResponse =
         withContext(Dispatchers.IO) {
             apiService.getPopularMovieList(BuildConfig.API_KEY)
         }
--- app/src/main/java/com/example/pruebaandroid/model/PoPularMovieResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pruebaandroid.model
-
-import com.google.gson.annotations.SerializedName
-
-data class PoPularMovieResponse(@SerializedName("results") val popularMovieList: List<PopularMovie>)
