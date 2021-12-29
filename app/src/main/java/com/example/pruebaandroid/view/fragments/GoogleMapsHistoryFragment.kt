7e043d4add956f52a17edf35da955d27ff80705d
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-29 17:21:24 -0600

    Implemented google maps

--- app/src/main/java/com/example/pruebaandroid/view/fragments/GoogleMapsHistoryFragment.kt
+++ app/src/main/java/com/example/pruebaandroid/view/fragments/GoogleMapsHistoryFragment.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pruebaandroid.R
 import com.example.pruebaandroid.databinding.FragmentGoogleMapsHistoryBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class GoogleMapsHistoryFragment : Fragment() {
 
@@ -22,5 +27,23 @@ class GoogleMapsHistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpGoogleMap(savedInstanceState)
+    }
+
+    private fun setUpGoogleMap(savedInstanceState: Bundle?) {
+        binding.googleMap.apply {
+            onCreate(savedInstanceState)
+            onResume()
+            getMapAsync(object : OnMapReadyCallback {
+                override fun onMapReady(map: GoogleMap) {
+                    val itspa = LatLng(19.54114297104868, -101.58399614481232)
+                    map.apply {
+                        addMarker(MarkerOptions().position(itspa).title("Marker"))
+                        moveCamera(CameraUpdateFactory.newLatLngZoom(itspa, 15f))
+                    }
+                }
+
+            })
+        }
     }
 }
