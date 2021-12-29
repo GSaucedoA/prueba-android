f456e452408db92bbb380be674050473752a4448
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-29 01:12:04 -0600

    Dato for database interactions

---
+++ app/src/main/java/com/example/pruebaandroid/businesslogic/database/dao/PopularMovieDao.kt
@@ -0,0 +1,17 @@
+package com.example.pruebaandroid.businesslogic.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.pruebaandroid.model.PopularMovie
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PopularMovieDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(movie: PopularMovie)
+
+    @Query("SELECT * FROM popular_movies")
+    fun getPopularMovies(): Flow<List<PopularMovie>>
+}
