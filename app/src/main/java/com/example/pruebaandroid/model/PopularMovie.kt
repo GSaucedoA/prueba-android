713de2c71d330d921080206687fa49eb78688760
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-29 12:02:50 -0600

    Added default values

--- app/src/main/java/com/example/pruebaandroid/model/PopularMovie.kt
+++ app/src/main/java/com/example/pruebaandroid/model/PopularMovie.kt
@@ -5,12 +5,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.pruebaandroid.businesslogic.utils.IMAGE_URL
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 @Entity(tableName = "popular_movies")
 data class PopularMovie(
-    @SerializedName("id") @PrimaryKey val id: Int,
-    @SerializedName("title") @ColumnInfo(name = "title") val title: String,
-    @SerializedName("poster_path") @ColumnInfo(name = "poster_path") val posterPath: String
+    @SerializedName("id") @PrimaryKey val id: Int = -1,
+    @SerializedName("title") @ColumnInfo(name = "title") val title: String = "",
+    @SerializedName("poster_path") @ColumnInfo(name = "poster_path") val posterPath: String = ""
 )
 
 fun PopularMovie.posterUrl(): String = IMAGE_URL.POSTER_W500.url + posterPath
