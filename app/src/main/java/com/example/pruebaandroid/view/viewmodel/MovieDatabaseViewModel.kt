7f7fd255b4a9998412a045ace56938a1f266a16a
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-29 15:17:09 -0600

    Refactor

--- app/src/main/java/com/example/pruebaandroid/view/viewmodel/MovieDatabaseViewModel.kt
+++ app/src/main/java/com/example/pruebaandroid/view/viewmodel/MovieDatabaseViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.pruebaandroid.view.viewmodel
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.pruebaandroid.businesslogic.database.dao.PopularMovieDao
 import com.example.pruebaandroid.businesslogic.respositories.PopularMovieApiRepo
 import com.example.pruebaandroid.model.PopularMovie
@@ -13,24 +16,17 @@ class MovieDatabaseViewModel @Inject constructor(
     private val repository: PopularMovieApiRepo,
     private val popularMovieDao: PopularMovieDao
 ) : ViewModel() {
-    private var _response = MutableLiveData<List<PopularMovie>>()
     val response: LiveData<List<PopularMovie>> = popularMovieDao.getPopularMovies().asLiveData()
-
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> get() = _isLoading
 
     init {
         getPopularMovies()
     }
 
     private fun getPopularMovies() = viewModelScope.launch {
-        _isLoading.value = true
         try {
             val list = repository.getPopularMovieList().popularMovieList
             popularMovieDao.insertList(list)
         } catch (erro: Throwable) {
-
         }
-        _isLoading.value = false
     }
 }
