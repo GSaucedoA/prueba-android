9d6df05999632acecc9d550c215969119e14e44a
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-29 15:14:54 -0600

    Added const values

--- app/src/main/java/com/example/pruebaandroid/businesslogic/utils/Const.kt
+++ app/src/main/java/com/example/pruebaandroid/businesslogic/utils/Const.kt
@@ -2,6 +2,14 @@ package com.example.pruebaandroid.businesslogic.utils
 
 import com.example.pruebaandroid.BuildConfig
 
+@JvmField
+val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
+    "Verbose WorkManager Notifications"
+const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
+    "Shows notifications whenever work starts"
+const val CHANNEL_ID = "VERBOSE_NOTIFICATION"
+const val NOTIFICATION_ID = 1
+
 enum class IMAGE_URL(val url: String) {
     POSTER_W500(BuildConfig.IMAGE_SERVER_URL + "w500")
 }
