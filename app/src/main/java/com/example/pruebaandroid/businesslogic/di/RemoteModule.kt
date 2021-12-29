003c3a0a56cdc2a1e92c7945c25af083944c7b7b
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-29 01:11:34 -0600

    Added room database and PopularMovieDao to dependency injection

--- app/src/main/java/com/example/pruebaandroid/businesslogic/di/RemoteModule.kt
+++ app/src/main/java/com/example/pruebaandroid/businesslogic/di/RemoteModule.kt
@@ -1,12 +1,16 @@
 package com.example.pruebaandroid.businesslogic.di
 
+import android.content.Context
 import com.example.pruebaandroid.BuildConfig
 import com.example.pruebaandroid.businesslogic.api.MovieDataBaseApiService
+import com.example.pruebaandroid.businesslogic.database.MovieDatabase
+import com.example.pruebaandroid.businesslogic.database.dao.PopularMovieDao
 import com.example.pruebaandroid.businesslogic.respositories.PopularMovieApiRepo
 import com.example.pruebaandroid.businesslogic.respositories.PopularMovieApiRepoImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -37,5 +41,15 @@ class RemoteModule {
         retrofit.create(MovieDataBaseApiService::class.java)
 
     @Provides
-    fun providesPopularMovieApiRepo(remoteService: MovieDataBaseApiService): PopularMovieApiRepo = PopularMovieApiRepoImpl(remoteService)
+    fun providesPopularMovieApiRepo(remoteService: MovieDataBaseApiService): PopularMovieApiRepo =
+        PopularMovieApiRepoImpl(remoteService)
+
+    @Singleton
+    @Provides
+    fun providesRoomDatabase(@ApplicationContext appContext: Context): MovieDatabase =
+        MovieDatabase.getDatabase(appContext)
+
+    @Provides
+    fun providesPopularMovieDao(database: MovieDatabase): PopularMovieDao =
+        database.popularMovieDao()
 }
