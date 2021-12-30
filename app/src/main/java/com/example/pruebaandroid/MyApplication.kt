be0ca30dca68249608eb1a0aeba5d041f76298c2
GSaucedoA <g.saucedo.kd@gmail.com>
2021-12-30 09:56:33 -0600

    Added worker factory

--- app/src/main/java/com/example/pruebaandroid/MyApplication.kt
+++ app/src/main/java/com/example/pruebaandroid/MyApplication.kt
@@ -1,8 +1,13 @@
 package com.example.pruebaandroid
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.pruebaandroid.businesslogic.Workers.LocationWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication : Application() {
+class MyApplication : Application(), Configuration.Provider {
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder().setWorkerFactory(LocationWorkerFactory(this)).build()
+    }
 }
